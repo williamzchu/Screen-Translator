537c20d9ff9b98f7a6254d42d8d19a2b94061b74
williamzchu@gmail.com <williamzchu@gmail.com>
2022-09-11 19:10:49 -0700

    Draw over other apps

--- Project/app/src/main/java/com/chufuyuan/screentranslator/MainActivity.java
+++ Project/app/src/main/java/com/chufuyuan/screentranslator/MainActivity.java
@@ -1,8 +1,11 @@
 package com.chufuyuan.screentranslator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkOverlayPermission();
+        startService();
+    }
+
+    // method for starting the service
+    public void startService(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // check if the user has already granted
+            // the Draw over other apps permission
+            if(Settings.canDrawOverlays(this)) {
+                // start the service based on the android version
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForegroundService(new Intent(this, com.chufuyuan.screentranslator.ForegroundService.class));
+                } else {
+                    startService(new Intent(this, com.chufuyuan.screentranslator.ForegroundService.class));
+                }
+            }
+        }else{
+            startService(new Intent(this, com.chufuyuan.screentranslator.ForegroundService.class));
+        }
+    }
+
+    // method to ask user to grant the Overlay permission
+    public void checkOverlayPermission(){
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                // send user to the device settings
+                Intent myIntent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+                startActivity(myIntent);
+            }
+        }
+    }
+
+    // check for permission again when user grants it from
+    // the device settings, and start the service
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startService();
     }
 }
